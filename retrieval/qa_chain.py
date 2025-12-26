589f2ed6675371dc9bde318efdcddce2472835bf
Rajat Singh <rajatsingh2774@gmail.com>
2025-12-26 11:58:44 +0530

    migrating to lanchain 1.0

--- retrieval/qa_chain.py
+++ retrieval/qa_chain.py
@@ -1,5 +1,5 @@
 from langchain_groq import ChatGroq
-from langchain.prompts import PromptTemplate
+from langchain_core.prompts import PromptTemplate
 from langchain_core.runnables import RunnablePassthrough
 from langchain_core.output_parsers import StrOutputParser
 import os
