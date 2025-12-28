050d1e3d36a10ac7665a7912f9765865d961a8be
Rajat Singh <rajatsingh2774@gmail.com>
2025-12-28 19:57:18 +0530

    added HF token

--- retrieval/retriever.py
+++ retrieval/retriever.py
@@ -1,10 +1,13 @@
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
+import os
 
 def create_vector_store(documents):
     embeddings = HuggingFaceEmbeddings(
         model_name="all-MiniLM-L6-v2",
-        model_kwargs={"device": "cpu"},
+        model_kwargs={"device": "cpu",
+                    "token": os.getenv("HF_TOKEN"),
+                      },
         encode_kwargs={
             "batch_size": 32,
             "normalize_embeddings": True
